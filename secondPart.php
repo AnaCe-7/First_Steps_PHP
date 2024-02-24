a09f986e68949e7e192623ed948d618080700f08
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-02-24 22:01:06 +0100

    Update secondPart exercises

--- secondPart.php
+++ secondPart.php
@@ -1,18 +1,44 @@
 <?php
 
-echo " <h4> ★ Truth tables: </h4>";
+echo '<button><a href="index.php">Home</a></button>';
 
-// Given the following variables
+echo " <h4> ★ Truth tables: </h4>";
 $es_un_michi_grande = true;
 $le_gusta_comer = true;
 $sabe_volar = false;
 $tiene_2_patas = false;
 
-// What is the result according to the truth tables?
+echo "<p>Given the following variables:</p>";
+echo "\$es_un_michi_grande = true;" . "<br>";
+echo "\$le_gusta_comer = true;" . "<br>";
+echo "\$sabe_volar = false;". "<br>";
+echo "\$tiene_2_patas = false;";
+
 $es_un_michi_grande && $le_gusta_comer; // true
 $es_un_michi_grande || $sabe_volar; // true
 $sabe_volar || $tiene_2_patas; // false
 !$le_gusta_comer; // false
 !$le_gusta_comer || $es_un_michi_grande; // true
+
+echo "<p>What is the result according to the truth tables?</p>";
+
+echo "\$es_un_michi_grande && \$le_gusta_comer;" . " ➜ ";
+var_dump($es_un_michi_grande && $le_gusta_comer);
+echo "<br>";
+
+echo "\$es_un_michi_grande || \$sabe_volar;" . " ➜ ";
+var_dump($es_un_michi_grande || $sabe_volar);
+echo "<br>";
+
+echo "\$sabe_volar || \$tiene_2_patas;" . " ➜ ";
+var_dump($sabe_volar || $tiene_2_patas);
+echo "<br>";
+
+echo "!\$le_gusta_comer;" . " ➜ ";
+var_dump(!$le_gusta_comer);
+echo "<br>";
+
+echo "!\$le_gusta_comer || \$es_un_michi_grande;" . " ➜ ";
+var_dump(!$le_gusta_comer || $es_un_michi_grande);
 
 ?>
